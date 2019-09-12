9ca28768235ca404db1c81f4ce5f2154be7403e1
javiert3 <54900272+javiert3@users.noreply.github.com>
2019-09-12 11:31:52 -0400

    Update connect.php

--- simple/connect.php
+++ simple/connect.php
@@ -16,6 +16,9 @@ $dbname = 'youtube';
   
 $conn = mysqli_init();
 mysqli_real_connect($conn, $host, $dbusername, $dbpassword, $dbname, 3306);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
 
 if (mysqli_connect_error()){
 die('Connect Error ('. mysqli_connect_errno() .') '
