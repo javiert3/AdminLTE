a55450b0b2e1ad4f63dc97cd502b09669590f134
javiert3 <54900272+javiert3@users.noreply.github.com>
2019-09-12 11:04:55 -0400

    Create create.php

---
+++ simple/create.php
@@ -0,0 +1,11 @@
+if (mysqli_query($conn, '
+CREATE TABLE Products (
+`Id` INT NOT NULL AUTO_INCREMENT ,
+`ProductName` VARCHAR(200) NOT NULL ,
+`Color` VARCHAR(50) NOT NULL ,
+`Price` DOUBLE NOT NULL ,
+PRIMARY KEY (`Id`)
+);
+')) {
+printf("Table created\n");
+}
